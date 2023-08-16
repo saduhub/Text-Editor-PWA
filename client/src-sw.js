d2aad834cf66e48e11a31c31f7d26601125adc86
Jorge Duarte <sadujorge@gmail.com>
2023-08-15 19:17:22 -0700

    Fixed image fetch bug, fixed db connection bug, refactored put and get

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -28,4 +29,19 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+// offlineFallback({
+//   pageFallback: '/index.html',
+//   imageFallback: '/images/icon.ico', 
+// });
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,11 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-const { GenerateSW } = require('workbox-webpack-plugin');
+// const { GenerateSW } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
@@ -21,39 +19,41 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    devServer: {
-      hot: 'only',
-    },
+    // devServer: {
+    //   hot: 'only',
+    // },
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Webpack Plugin',
+        title: 'Text Editor',
       }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
       }),
-      new MiniCssExtractPlugin(),
-      new GenerateSW({
-        // Do not precache images
-        exclude: [/\.(?:png|jpg|jpeg|svg)$/],
-        // Define runtime caching rules.
-        runtimeCaching: [{
-          // Match any request that ends with .png, .jpg, .jpeg or .svg.
-          urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-          // Apply a cache-first strategy.
-          handler: 'CacheFirst',
-          options: {
-            // Use a custom cache name.
-            cacheName: 'images',
-            // Only cache 2 images.
-            expiration: {
-              maxEntries: 2,
-            },
-          },
-        }],
-      }),
+      // new MiniCssExtractPlugin(),
+      // new GenerateSW({
+      //   // Do not precache images
+      //   exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+      //   // Define runtime caching rules.
+      //   runtimeCaching: [{
+      //     // Match any request that ends with .png, .jpg, .jpeg or .svg.
+      //     urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+      //     // Apply a cache-first strategy.
+      //     handler: 'CacheFirst',
+      //     options: {
+      //       // Use a custom cache name.
+      //       cacheName: 'images',
+      //       // Only cache 2 images.
+      //       expiration: {
+      //         maxEntries: 2,
+      //       },
+      //     },
+      //   }],
+      // }),
       new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
         name: 'Just Another Text Editor',
         short_name: 'JATE',
         description: 'Note taker wih JS highlighting',
@@ -63,9 +63,9 @@ module.exports = () => {
         publicPath: './',
         icons: [
           {
-            src: path.resolve('./favicon.ico'),
+            src: path.resolve('./src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('images'),
+            destination: path.join('assets', 'icons'),
           },
         ],
       }),
@@ -75,12 +75,13 @@ module.exports = () => {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          // use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: ['style-loader', 'css-loader'],
         },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
+        // {
+        //   test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        //   type: 'asset/resource',
+        // },
         {
           test: /\.m?js$/,
           exclude: /(node_modules|bower_components)/,
@@ -88,6 +89,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
             },
           },
         },
